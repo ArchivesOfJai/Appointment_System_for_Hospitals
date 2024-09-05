354a9cc6acd8b27a01c48fa480e453f4d9a5b860
Jai Prakash Singh <prakash.jaiisingh@gmail.com>
2024-09-05 21:42:37 +0530

    fixed deploying

--- src/components/Navbar/NavLinks.jsx
+++ src/components/Navbar/NavLinks.jsx
@@ -7,19 +7,19 @@ function NavLinks() {
   return (
     <ul className='navbar-nav  mx-auto'>
       <li className='nav-item mx-2'>
-        <a href="/" className={`nav-link ${window.location.pathname === '/' ? 'active' : ''} `}>Home</a>
+        <a href="/Appointment_System_for_Hospitals/" className={`nav-link ${window.location.pathname === '/Appointment_System_for_Hospitals/' ? 'active' : ''} `}>Home</a>
       </li>
       <Divider orientation='vertical' flexItem className='mx-2' />
       <li className='nav-item mx-2'>
-        <a href="/Services" className={`nav-link ${window.location.pathname === '/Services' ? 'active' : ''} `} >Services</a>
+        <a href="/Appointment_System_for_Hospitals/Services" className={`nav-link ${window.location.pathname === '/Appointment_System_for_Hospitals/Services' ? 'active' : ''} `} >Services</a>
       </li>
       <Divider orientation='vertical' flexItem className='mx-2' />
       <li className='nav-item mx-2'>
-        <a href="/FindDoctor" className={`nav-link ${window.location.pathname === '/FindDoctor' ? 'active' : ''} `} >Find a doctor</a>
+        <a href="/Appointment_System_for_Hospitals/FindDoctor" className={`nav-link ${window.location.pathname === '/Appointment_System_for_Hospitals/FindDoctor' ? 'active' : ''} `} >Find a doctor</a>
       </li>
       <Divider orientation='vertical' flexItem className='mx-2' />
       <li className='nav-item mx-2'>
-        <a href="/About" className={`nav-link ${window.location.pathname === '/About' ? 'active' : ''} `} >About us</a>
+        <a href="/Appointment_System_for_Hospitals/About" className={`nav-link ${window.location.pathname === '/Appointment_System_for_Hospitals/About' ? 'active' : ''} `} >About us</a>
       </li>
       <Divider orientation='vertical' flexItem className='mx-2' />
     </ul>
