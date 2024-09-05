74b96464c25a7a91cffbb71a74239787a19a6f89
Jai Prakash Singh <prakash.jaiisingh@gmail.com>
2024-09-05 21:35:15 +0530

    vite.config updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import Navbar from './components/Navbar/Navbar';
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/HomePage/Home';
 
 function App() {
   return(
-    <>
-    <Navbar/>
-    </>
+    <Router>
+        <Navbar /> 
+        <div style={{height: '57px',width: '100%'}}></div>
+        <Routes>
+          <Route path="/" element={<Home />} />        
+        
+        </Routes>
+    </Router>
   )
 }
 
